fee78e235b94062d3e9025e97ed18393c81ece64
rasikag <rasikagayan90@gmail.com>
2018-02-02 22:11:14 +0800

    added router to idea

--- app.js
+++ app.js
@@ -75,90 +75,15 @@ app.get('/about' , (req, res)=>{
   res.render('about');
 });
 
-// ideas index page 
-app.get('/ideas',(req,res)=> {
-  Idea.find({})
-      .sort({date:'desc'})
-      .then(ideas => {
-          res.render('ideas/index',{
-              ideas: ideas
-          });
-      });
+
+
+// User login route
+app.get('/users/login',(req, res) => {
+  res.send('login');
 });
 
-// show idea form 
-// here you can add a idea to application
-app.get('/ideas/add', (req, res)=>{
-  res.render('ideas/add');
-});
+app.get('/users/register',(req, res)=>{
 
-// save the idea 
-app.post('/ideas',(req,res)=>{
-  let errors = [];
-  if(!req.body.title){
-      errors.push({text: 'Please add a title'});
-  }
-  if(!req.body.details){
-      errors.push({text: 'Please add some details'});
-  }
-  if(errors.length > 0){
-      res.render('ideas/add',{
-          errors : errors,
-          title : req.body.title,
-          details : req.body.details
-      });
-  } else {
-      const newUser = {
-          title : req.body.title,
-          details : req.body.details
-      }
-      new Idea(newUser)
-          .save()
-          .then(idea =>{
-              req.flash('success_msg', 'Video idea added ');
-              res.redirect('/ideas');
-          })
-  }
-});
-
-// Edit ideas
-app.get('/ideas/edit/:id', (req, res)=>{
-  Idea.findOne({
-    _id : req.params.id
-  })
-  .then(idea => {
-    res.render('ideas/edit', {
-      idea : idea
-    });
-  });
-});
-
-// Submit edit form data
-app.put('/ideas/:id',(req,res)=>{
-  Idea.findOne({
-    _id : req.params.id
-  })
-  .then(idea =>{
-    // once find a record it will return it
-    idea.title = req.body.title;
-    idea.details = req.body.details;
-
-    idea.save()
-        .then(idea => {
-          req.flash('success_msg', 'Video idea updated');
-          res.redirect('/ideas');
-        });
-  });
-});
-
-// Delete idea
-app.delete('/ideas/:id',(req,res)=>{
-  Idea.remove({
-    _id : req.params.id
-  }).then(() => {
-    req.flash('success_msg', 'Video idea removed ');
-    res.redirect('/ideas');
-  });
 });
 
 app.listen(port , () => {

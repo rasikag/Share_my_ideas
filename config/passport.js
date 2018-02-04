2b5dd3c95cc6ac3c673effab5a391f6e5a998f87
rasikag <rasikagayan90@gmail.com>
2018-02-04 22:39:21 +0800

    added passport session

--- config/passport.js
+++ config/passport.js
@@ -5,19 +5,46 @@ const bcrypt = require('bcryptjs');
 // Load user model 
 const User = mongoose.model('users');
 
-module.exports = function(passport){
+module.exports = function (passport) {
     passport.use(new LocalStrategy(
-                    {usernameField : 'email'},
-                    (email,password,done) => {
-                         User.findOne({
-                             email : email
-                         }).then(user => {
-                             if(!user){
-                                return done(null, 
-                                            false, 
-                                            {message : 'No user found'});
-                             }
-                         });
-                    }
+        { usernameField: 'email' },
+        (email, password, done) => {
+
+            // Match user 
+            User.findOne({
+                email: email
+            }).then(user => {
+                // if user not found 
+                if (!user) {
+                    return done(null,
+                        false,
+                        { message: 'No user found' });
+                }
+
+                // if user found 
+                // then match the password
+                bcrypt.compare(password, user.password,
+                    (err, isMatch) => {
+                        if (err) throw err;
+                        if (isMatch) {
+                            return done(null, user);
+                        } else {
+                            return done(null,
+                                false,
+                                { message: 'Password incorrect' });
+                        }
+                    })
+            });
+        }
     ));
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
 }
--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.get('/login',(req, res) => {
 router.post('/login', (req , res, next) => {
     passport.authenticate('local', {
         successRedirect: '/',
-        failureRedirect: '/login',
+        failureRedirect: 'login',
         failureFlash: true
     })(req, res, next);
 });

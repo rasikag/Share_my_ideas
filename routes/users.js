0074ccd9e554b0da26f19ebf79ac001c1a80cbca
rasikag <rasikagayan90@gmail.com>
2018-02-04 12:50:04 +0800

    added user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const passport = require('passport')
+
+// Load user model
+require('../models/User');
+const User = mongoose.model('users');
 
 // User login route
 router.get('/login',(req, res) => {
@@ -29,7 +35,28 @@ router.post('/register' , (req,res)=>{
             email : req.body.email
         });
     } else {
-        res.send('pass');
+        const newUser = new User({
+            name : req.body.name,
+            email : req.body.email,
+            password : req.body.email
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password , salt, (err, hash) => {
+                if(err) throw err;
+                newUser.password = hash;
+                newUser.save()
+                    .then(user => {
+                        req.flash('success_msg', 
+                        'You are successfully registered to the system');
+                        res.redirect('/users/login');
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        return;
+                    });
+            });
+        });
     }
     
 });
